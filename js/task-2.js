8005d628226390d2e6df185d0996667a40e10833
Ruslan Shevchenko <runya_@ukr.net>
2024-09-28 16:19:47 +0300

    added task-2.js

---
+++ js/task-2.js
@@ -0,0 +1,22 @@
+function formatMessage(message, maxLength) {
+  let strSlice;
+  if (message.length <= maxLength) {
+    return message;
+  } else {
+    strSlice = message.slice(0, maxLength);
+  }
+  return `${strSlice}...`;
+}
+formatMessage("Curabitur ligula sapien", 16);
+formatMessage("Curabitur ligula sapien", 23);
+formatMessage("Vestibulum facilisis purus nec", 20);
+formatMessage("Vestibulum facilisis purus nec", 30);
+formatMessage("Nunc sed turpis a felis in nunc fringilla", 15);
+formatMessage("Nunc sed turpis a felis in nunc fringilla", 41);
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
